58e8c657c1c3157a4d2725f64c3dec8181822456
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-24 19:11:05 +0200

    Update installIruinWebcam.sh

--- Apps/installIruinWebcam.sh
+++ Apps/installIruinWebcam.sh
@@ -1,6 +1,6 @@
-curl https://iriun.gitlab.io/iriunwebcam-2.6.deb
+curl -o https://iriun.gitlab.io/iriunwebcam-2.6.deb
 wait
-cd Downloads
+cd ~/Downloads
 sudo alien -r iriunwebcam-2.6.deb --target=x86_64
 wait
 sudo dnf install iriunwebcam-2.6.x86_64.rpm -y
