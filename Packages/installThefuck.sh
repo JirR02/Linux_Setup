7cf08eb7f972840593d10a7f2715403639ef618b
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-24 19:24:44 +0200

    Update installThefuck.sh

--- Packages/installThefuck.sh
+++ Packages/installThefuck.sh
@@ -1,6 +1,6 @@
-curl https://kojipkgs.fedoraproject.org//packages/thefuck/3.32/2.fc36/src/thefuck-3.32-2.fc36.src.rpm
+curl -o https://kojipkgs.fedoraproject.org//packages/thefuck/3.32/2.fc36/src/thefuck-3.32-2.fc36.src.rpm
 wait
-cd Downloads
+cd ~/Downloads
 sudo dnf install thefuck-3.32-2.fc36.src.rpm
 wait
 rm thefuck-3.32-2.fc36.src.rpm
