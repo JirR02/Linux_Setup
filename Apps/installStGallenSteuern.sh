f4aa1aaebb35bca3cb3d9273bed7181f35494f66
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-24 19:15:16 +0200

    Update installStGallenSteuern.sh

--- Apps/installStGallenSteuern.sh
+++ Apps/installStGallenSteuern.sh
@@ -1,5 +1,5 @@
 curl -o https://steuersoftware.sg.oca.ch/Steuern_2021/SGnP2021_installieren_unix_64bit.sh
 wait
-cd Downloads
+cd ~/Downloads
 chmod +x Steuern_2021/SGnP2021_installieren_unix_64bit.sh
 Steuern_2021/SGnP2021_installieren_unix_64bit.sh
