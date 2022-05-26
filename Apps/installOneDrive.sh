dc44868380ac1928ee7cd746689c83e74690098f
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-26 10:57:34 +0200

    Update installOneDrive.sh

--- Apps/installOneDrive.sh
+++ Apps/installOneDrive.sh
@@ -1,6 +1,6 @@
+cd /home/jirayuruh/Downloads
 curl -o https://kojipkgs.fedoraproject.org//packages/onedrive/2.4.17/1.fc35/src/onedrive-2.4.17-1.fc35.src.rpm
 wait
-cd ~/Downloads
-sudo dnf install onedrive-2.4.17-1.fc35.src.rpm -y
+find ./ -tyoe f -name "*.rpm" -exec sudo dnf install {} \;
 wait
-rm onedrive-2.4.17-1.fc35.src.rpm
+find ./ -tyoe f -name "*.rpm" -exec rm {} \;
