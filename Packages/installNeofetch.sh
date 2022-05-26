aef82911869e6f6ccdbb4a3602d955aa8f423680
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-26 11:11:14 +0200

    Update installNeofetch.sh

--- Packages/installNeofetch.sh
+++ Packages/installNeofetch.sh
@@ -1,6 +1,6 @@
-curl -o https://kojipkgs.fedoraproject.org//packages/neofetch/7.1.0/7.fc36/src/neofetch-7.1.0-7.fc36.src.rpm
+cd /home/jirayuruh/Downloads
+curl -O https://kojipkgs.fedoraproject.org//packages/neofetch/7.1.0/7.fc36/src/neofetch-7.1.0-7.fc36.src.rpm
 wait
-cd ~/Downloads
-sudo dnf install neofetch-7.1.0-7.fc36.src.rpm -y
+find -type f -name "*.rpm" -exec sudo dnf install {} -y \;
 wait
-rm neofetch-7.1.0-7.fc36.src.rpm
+find -type f -name "*.rpm" -exec rm {} \;
