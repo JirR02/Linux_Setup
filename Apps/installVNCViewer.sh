c2e78151bf951aea75fe02efe69e90ce181b32be
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-26 11:03:22 +0200

    Update installVNCViewer.sh

--- Apps/installVNCViewer.sh
+++ Apps/installVNCViewer.sh
@@ -1,6 +1,6 @@
-curl -o https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-6.22.315-Linux-x86.rpm
+cd /home/jirayuruh/Downloads
+curl -O https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-6.22.315-Linux-x86.rpm
 wait
-cd ~/Downloads
-sudo dnf install VNC-Viewer-6.22.315-Linux-x86.rpm -y
+find ./ -type f -name "*.rpm" -exec sudo dnf install {} -y \;
 wait
-rm VNC-Viewer-6.22.315-Linux-x86.rpm
+find ./ -type f -name "*.rpm" -exec rm {} \;
