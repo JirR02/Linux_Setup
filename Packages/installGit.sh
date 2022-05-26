e933d670cc1ccb168fc3132bdf74d29b7dd60ff5
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-26 11:08:25 +0200

    Update installGit.sh

--- Packages/installGit.sh
+++ Packages/installGit.sh
@@ -1,8 +1,8 @@
 sudo dnf -y install git
 wait
-cd ~/Downloads
-curl -o https://kojipkgs.fedoraproject.org//packages/git-lfs/3.1.2/1.fc36/src/git-lfs-3.1.2-1.fc36.src.rpm
-sudo dnf install git-lfs-3.1.2-1.fc36.src.rpm -y
+cd /home/jirayuruh/Downloads
+curl -O https://kojipkgs.fedoraproject.org//packages/git-lfs/3.1.2/1.fc36/src/git-lfs-3.1.2-1.fc36.src.rpm
+find -type f -name "*.rpm" -exec sudo dnf install {} -yes \;
 wait
-rm git-lfs-3.1.2-1.fc36.src.rpm
+find -type f -name "*.rpm" -exec rm {} \;
 git lfs install
