3c0ac5ecc2aaf213a650b75d3ad8684d1f7b4667
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-26 11:13:28 +0200

    Update installQpdf.sh

--- Packages/installQpdf.sh
+++ Packages/installQpdf.sh
@@ -1,6 +1,6 @@
-curl -o https://kojipkgs.fedoraproject.org//packages/qpdf/10.6.3/1.fc36/src/qpdf-10.6.3-1.fc36.src.rpm
+cd /home/jirayuruh/Downloads
+curl -O https://kojipkgs.fedoraproject.org//packages/qpdf/10.6.3/1.fc36/src/qpdf-10.6.3-1.fc36.src.rpm
 wait
-cd ~/Downloads
-sudo dnf install qpdf-10.6.3-1.fc36.src.rpm
+find ./ -type f -name "*.rpm" -exec sudo dnf install {} -yes \;
 wait
-rm qpdf-10.6.3-1.fc36.src.rpm
+find ./ -type f -name "*.rpm" -exec rm {} \;
