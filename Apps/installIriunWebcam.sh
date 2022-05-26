920840053ac1bb5bfe13dfba20ad80df60657d13
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-05-26 10:48:51 +0200

    Delete installIriunWebcam.sh

--- Apps/installIriunWebcam.sh
+++
@@ -1,9 +0,0 @@
-curl -o https://iriun.gitlab.io/iriunwebcam-2.6.deb
-wait
-cd ~/Downloads
-sudo alien -r iriunwebcam-2.6.deb --target=x86_64
-wait
-sudo dnf install iriunwebcam-2.6.x86_64.rpm -y
-wait
-rm iriunwebcam-2.6.deb
-rm iriunwebcam-2.6.x86_64.rpm
