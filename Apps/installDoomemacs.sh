214724eb2c1e4f8f79b56bb44db8f82a1c8c379b
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-06-10 23:46:12 +0200

    Update and rename installDoomemacs.sh to installDoomnvim.sh

--- Apps/installDoomemacs.sh
+++
@@ -1,2 +0,0 @@
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-sudo ~/.emacs.d/bin/doom install
---
+++ Apps/installDoomnvim.sh
@@ -0,0 +1,1 @@
+git clone --depth 1 https://github.com/NTBBloodbath/doom-nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
