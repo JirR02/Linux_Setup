4e9b2a440d0da36a4de310f9008479887ca6179b
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-06-11 15:46:31 +0200

    Update installDoomnvim.sh

--- Apps/installDoomnvim.sh
+++ Apps/installDoomnvim.sh
@@ -1,1 +1,5 @@
+sudo pacman -S --noconfirm --needed base-devel
+wait
+sudo yay -S --noconfirm --needed libstdc++5
+wait
 git clone --depth 1 https://github.com/NTBBloodbath/doom-nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
