3a0f9346571e61064946f94a3230eadf6d24a60b
Jirayu Ruh <89262682+JirR02@users.noreply.github.com>
2022-06-11 21:41:28 +0200

    Update installxmonad.sh

--- XMonad/installxmonad.sh
+++ XMonad/installxmonad.sh
@@ -1,4 +1,3 @@
-sudo pacman -S --noconfirm --needed xorg lightdm lightdm-gtk-greeter xmonad xmonad-contrib xmobar 
+sudo pacman -S --noconfirm --needed xmonad xmonad-contrib xmobar 
 wait
-yay -S --noconfirm --needed lightdm-webkit-theme-ether
 mkdir /home/jirayuruh/.xmonad
